08fdb754cf43c0331538647087dc2bf68bda415a
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2024-03-11 12:47:27 +0530

    game controls working good
    start button clicks working and get random board

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('game starting')
+
+const game = document.querySelector('#game')
+const pointDisplay = document.querySelector('#point-display')
+const timeDisplay = document.querySelector('#time-display')
+
+const allBoards = document.querySelectorAll('.board')
+const btn = document.querySelector('.btn')
+
+// when the stat game button clicks game started
+btn.addEventListener('mousedown', function() {
+   gameStarting()
+})
+
+
+function gameStarting() {
+   allBoards.forEach((board) => {
+      board.classList.remove('mole')
+   })
+
+   let randomBoard = allBoards[Math.floor(Math.random() * allBoards.length)]
+   randomBoard.classList.add('mole')
+   console.log(randomBoard)
+}
